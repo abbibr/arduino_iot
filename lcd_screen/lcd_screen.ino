89ecada2370356f87fc967ce96c884e478de9275
abbibr <ibrohim_abbosov@mail.ru>
2024-10-27 11:49:03 +0500

    arduino iot cloud led thing using esp32 wifi 1010

--- lcd_screen/lcd_screen.ino
+++ lcd_screen/lcd_screen.ino
@@ -1,24 +1,15 @@
-/*
-  GND to GND on the Arduino.
-  VCC to 5V on the Arduino.
-  SDA to A4 on the Arduino (on most Arduino boards)
-  SCL to A5 on the Arduino (on most Arduino boards)
-*/
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Replace 0x27 with your detected I2C address if different
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.backlight();  // Turn on the backlight if your LCD has it
-  lcd.setCursor(0, 0);  // Set cursor to first column, first row
-  lcd.print("Hello, World!");  // Print text on the first row
-
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Testing LCD");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Empty loop
 }

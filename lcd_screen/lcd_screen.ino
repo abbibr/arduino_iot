5b9f8768428870530287d3986a4257d5a8d983c4
abbibr <ibrohim_abbosov@mail.ru>
2024-10-31 00:04:18 +0500

    lcd screen codes updated for arduino esp32 (wifi mkr 1010)

--- lcd_screen/lcd_screen.ino
+++ lcd_screen/lcd_screen.ino
@@ -1,30 +1,23 @@
-/*
-  GND to GND on the Arduino.
-  VCC to 5V on the Arduino.
-  SDA to A4 on the Arduino (on most Arduino boards)
-  SCL to A5 on the Arduino (on most Arduino boards)
-*/
-
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Set the LCD address to 0x27 for a 20x4 display
-LiquidCrystal_I2C lcd(0x27, 20, 4);  // Replace 0x27 with your LCD's I2C address if necessary
+LiquidCrystal_I2C lcd(0x27, 20, 4); // Replace 0x27 with the detected address
 
 void setup() {
-  lcd.begin(20, 4);  // Initialize the LCD with 20 columns and 4 rows
-  lcd.backlight();    // Turn on the backlight
-  lcd.setCursor(0, 0);  // Set cursor to first column, first row
-  lcd.print("Hi everone!");  // Print text on the first row
-  lcd.setCursor(3, 1);  // Set cursor to first column, first row
-  lcd.print("Ibrohim Abbosov - ");  // Print text on the first row
-  lcd.setCursor(0, 2);  // Set cursor to first column, first row
-  lcd.print("CEO of abbibr compan");  // Print text on the first row
-  lcd.setCursor(0, 3);  // Set cursor to first column, first row
-  lcd.print("Team Leader ");  // Print text on the first row
+  Wire.begin(21, 22);  // Set ESP32 I2C pins
+  Wire.setClock(100000);
+  lcd.init();    
+  lcd.backlight(); 
+  lcd.setCursor(0, 0); 
+  lcd.print("Hello, CEO!");
+  lcd.setCursor(0, 1); 
+  lcd.print("Welcome to th abbibr");
+  lcd.setCursor(0, 2); 
+  lcd.print("Happy to see you");
+  lcd.setCursor(3, 3); 
+  lcd.print("Byeee!)");
 }
 
 void loop() {
-  // You can add more code here if needed, or leave the loop empty for static text
+  // No code needed here
 }
